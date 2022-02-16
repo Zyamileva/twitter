8e4ad0d500b751528601e377eb1b06429f4a49fb
Lena Zyamileva <elenazyamileva@gmail.com>
2022-02-17 00:34:58 +0200

    Changed Homework11

--- src/main/java/org/zyamileva/twitter/Twitter.java
+++ src/main/java/org/zyamileva/twitter/Twitter.java
@@ -13,7 +13,6 @@ public class Twitter {
         System.out.println(Context.getInstance().getFeedService().buildReplyFeed(UUID.fromString("abad2161-da52-443b-aa3a-33556fcd18db")));
         System.out.println(Context.getInstance().getFeedService().buildHomeFeed(UUID.fromString("e0988b42-6b87-42ff-ab63-61ad5b621f8d")));
         System.out.println(Context.getInstance().getFeedService().buildUserFeed(UUID.fromString("03dccf47-6d9b-4835-847b-fcd4dff93c8e")));
-
 //        System.out.println(Context.getInstance().getFeedService().buildHomeFeed(Context.getInstance().getUserService().findByLogin("@anna").orElseThrow().getId()));
 //        System.out.println(Context.getInstance().getFeedService().buildUserFeed(Context.getInstance().getUserService().findByLogin("@anna").orElseThrow().getId()));
     }
