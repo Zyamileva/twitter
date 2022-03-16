655f47fb11177f7a9a952802de0d468bf4a1aecd
Lena Zyamileva <elenazyamileva@gmail.com>
2022-03-16 16:04:02 +0200

    Corrected Homework13

--- src/main/java/org/zyamileva/twitter/Twitter.java
+++ src/main/java/org/zyamileva/twitter/Twitter.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 public class Twitter {
     public static void main(String[] args) {
         ApplicationRunner.run(args);
+
 //        System.out.println(Context.getInstance().getFeedService().buildReplyFeed(UUID.fromString("abad2161-da52-443b-aa3a-33556fcd18db")));
 //        System.out.println(Context.getInstance().getFeedService().buildHomeFeed(UUID.fromString("e0988b42-6b87-42ff-ab63-61ad5b621f8d")));
 //        System.out.println(Context.getInstance().getFeedService().buildUserFeed(UUID.fromString("03dccf47-6d9b-4835-847b-fcd4dff93c8e")));
